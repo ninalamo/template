a5a24b8794e5b56e73dd5987c776012b02aeb715
chrisdan.evalla@outlook.com <chrisdanevalla@gmail.com>
2023-08-28 01:23:09 +0800

    remove use-client in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-'use client'
-
-import Nav from '@/components/Nav'
+import NavWrapper from '@/components/NavWrapper';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Fira_Sans } from 'next/font/google';
-import { usePathname } from 'next/navigation';
 
 
 const firaSans = Fira_Sans({
@@ -24,27 +21,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const pathName = usePathname();
-
   
   return (
     <html lang="en">
       <body className={firaSans.className}>
         <div className="bg-gray-100 w-full min-h-screen m-0">
-
-          {
-            !pathName.includes('/ext/v1/member')? 
-            <>
-              <Nav />
-              <div className="container mx-auto px-4 py-4 mt-3">
-                {children}
-              </div>
-            </>
-            : 
-            <div>
-                {children}
-            </div>
-          }
+            <NavWrapper>{children}</NavWrapper>          
         </div>
       </body>
     </html>
